80c529860e5d8b7b1435896e9e04d28a925cb3d8
root <root@LAPTOP-33N12T6F.>
2025-02-04 15:20:54 -0500

    main function is added with whole code

--- main_script.sh
+++ main_script.sh
@@ -2,8 +2,14 @@
 
 
 # Call the function script to calculate m
-RESULT=$(./calculate_m.sh "$1" "$2" "$3")
+RESULT=$(./calculate_m.sh 10 5 2)
 
 # Print the result
 echo "The calculated slope (m) is: $RESULT"
 
+
+# Check if exactly 3 arguments are provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <10> <5> <2>"
+    exit 1
+fi
